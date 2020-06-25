3326d540b2a60e8a6e2da63a591b1a008a03a1b3
Edwin Sarabia <WRzSarvia@hotmail.com>
2020-06-25 08:32:41 -0600

    Added Class PongBola

---
+++ Juego_ping_pong/PongBola.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juego_ping_pong
+{
+    class PongBola: PongOrigen
+    {
+        public void Actualizar()
+        {
+            Posicion = new Point(Posicion.X + Velocidad.X, Posicion.Y + Velocidad.Y);
+        }
+    }
+}

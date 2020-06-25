093b281a442a9c575ac016c017c8525e393ad6cd
Edwin Sarabia <WRzSarvia@hotmail.com>
2020-06-25 08:24:46 -0600

    adding class PongOrigen

---
+++ Juego_ping_pong/PongOrigen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Juego_ping_pong
+{
+    class PongOrigen
+    {
+        public Point Origen
+        {
+            get
+            {
+                return new Point(Texturas.Width / 2 , Texturas.Height / 2);
+            }
+        }
+        public PictureBox Texturas { get; set; }
+        public Point Posicion { get; set; }
+        public Point Velocidad { get; set; }
+        public Point EsquIzquierda
+        {
+            get { return new Point(Posicion.X - Origen.X, Posicion.Y - Origen.Y); }
+        }
+        public Point EsqDerecha
+        {
+            get { return new Point(Posicion.X + Origen.X, Posicion.Y - Origen.Y); }
+        }
+        public Point EsqInferiorDerecha
+        {
+            get { return new Point(Posicion.X - Origen.X, Posicion.Y + Origen.Y); }
+        }
+        public Point RightBottomCorner
+        {
+            get { return new Point(Posicion.X + Origen.X, Posicion.Y + Origen.Y); }
+        }
+        public void Draw()
+        {
+            Texturas.Location = new Point(Posicion.X - Origen.X,Posicion.Y - Origen.Y);
+        }
+    }
+}
